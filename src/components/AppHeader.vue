831f2dabc80a72ae407e425802ff91e2a220c112
Felipe Henrich <henrich.felipe@gmail.com>
2020-03-20 13:02:29 -0300

    style: AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,3 +11,19 @@ export default {
     </h1>
   </div>
 </template>
+
+<style scoped>
+.base-header {
+  background: #268700;
+  color: #fbe500;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 150px;
+}
+
+.base-header h1 {
+  font-size: 50px;
+  text-transform: uppercase;
+}
+</style>
