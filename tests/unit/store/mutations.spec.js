7a18bd65fd4d74f2ae26657db7550436e9021be3
Felipe Henrich <henrich.felipe@gmail.com>
2020-04-09 01:31:34 -0300

    fix: removed SET_DECK_ID as is not needed

--- tests/unit/store/mutations.spec.js
+++ tests/unit/store/mutations.spec.js
@@ -3,7 +3,6 @@ import { reorderedDeck } from "../testCards";
 
 const state = {
   deck: [],
-  deckId: "",
   pickedCards: [],
   rotationCard: "",
   suits: ["S", "C", "D", "H"],
@@ -11,14 +10,11 @@ const state = {
 };
 
 describe("Mutations", () => {
-  it("sets deck id for the deckofcards api", () => {
-    mutations.SET_DECK_ID(state, "qwerty123");
-    expect(state.deckId).toBe("qwerty123");
-  });
   it("sets rotation card", () => {
     mutations.PICK_ROTATION_CARD(state, "6C");
     expect(state.rotationCard).toBe("6C");
   });
+
   it("sets cards from user pile", () => {
     mutations.PICK_CARDS(state, ["2H", "2D", "2C", "2C", "3H", "3D", "3C"]);
     expect(state.pickedCards).toStrictEqual([
@@ -31,6 +27,7 @@ describe("Mutations", () => {
       "3C"
     ]);
   });
+
   it("reorder ranks and suits position based on the rotation card", () => {
     mutations.REORDER_WITH_ROTATION_CARD(state);
     const { suits, ranks } = state;
@@ -39,6 +36,7 @@ describe("Mutations", () => {
       ranks: ["7", "8", "9", "10", "J", "Q", "K", "A", "2", "3", "4", "5", "6"]
     });
   });
+
   it("stacks deck order based on the rank and suits highest position in their respective arrays", () => {
     mutations.STACK_DECK(state);
     expect(state.deck).toStrictEqual(reorderedDeck);
